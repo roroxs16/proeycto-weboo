e63e38a5a1757f424763d1e723788a39acf60b5c
roroxs16 <38821332+roroxs16@users.noreply.github.com>
2019-09-28 22:49:28 -0600

    FIrst commit

---
+++ aplicacionposeidon/src/main/java/com/poseidonapp/prototipo/AplicacionposeidonApplication.java
@@ -0,0 +1,13 @@
+package com.poseidonapp.prototipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionposeidonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionposeidonApplication.class, args);
+	}
+
+}
