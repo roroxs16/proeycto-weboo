a6cc0c2cd31e023ba8aec121fb89ffc08472cac7
Pilar Gonzalez <44037584+piloGC@users.noreply.github.com>
2019-12-16 10:25:33 -0300

    [UPDATE] interfaz version pili

--- aplicacionposeidon/src/main/java/com/poseidonapp/prototipo/AplicacionposeidonApplication.java
+++ aplicacionposeidon/src/main/java/com/poseidonapp/prototipo/AplicacionposeidonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AplicacionposeidonApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AplicacionposeidonApplication.class, args);
 	}
